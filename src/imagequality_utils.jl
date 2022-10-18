4a93d7683e5fcbb1743439161d12833ec8bfafe8
Gabrio Rizzuti <rizzuti.gabrio@gmail.com>
2022-10-18 15:11:04 +0200

    Updates

--- src/imagequality_utils.jl
+++ src/imagequality_utils.jl
@@ -1,6 +1,31 @@
 # Image-quality metrics
 
-export psnr#, ssim
+export psnr, ssim
 
-psnr(u_noisy::AbstractArray{T,N}, u_ref::AbstractArray{T,N}; preproc::Function=x->abs.(x)) where {T,N} = 10*log10(norm(preproc(u_ref), Inf)^2/(norm(preproc(u_noisy)-preproc(u_ref))^2/prod(size(u_noisy))))
-# ssim(u_noisy::AbstractArray{T,N}, u_ref::AbstractArray{T,N}; preproc::Function=x->abs.(x)) where {T,N} = assess_ssim(preproc(u_noisy), preproc(u_ref))
+function psnr(u_noisy::AbstractArray{T,N}, u_ref::AbstractArray{T,N}; slices::Union{Nothing,NTuple{<:Any,VolumeSlice}}, orientation::Orientation=standard_orientation()) where {T<:Real,N}
+    if isnothing(slices)
+        x, y, z = div.(size(u_noisy), 2).+1
+        slices = (VolumeSlice(1, x), VolumeSlice(2, y), VolumeSlice(3, z))
+    end
+    psnr_slice = similar(u_noisy, length(slices))
+    @inbounds for (i, slice) = enumerate(slices)
+        u_noisy_slice = select(u_noisy, slice; orientation=orientation)
+        u_ref_slice = select(u_ref, slice; orientation=orientation)
+        psnr_slice[i] = assess_psnr(u_noisy_slice, u_ref_slice)
+    end
+    return tuple(psnr_slice...)
+end
+
+function ssim(u_noisy::AbstractArray{T,N}, u_ref::AbstractArray{T,N}; slices::Union{Nothing,NTuple{<:Any,VolumeSlice}}, orientation::Orientation=standard_orientation()) where {T<:Real,N}
+    if isnothing(slices)
+        x, y, z = div.(size(u_noisy), 2).+1
+        slices = (VolumeSlice(1, x), VolumeSlice(2, y), VolumeSlice(3, z))
+    end
+    ssim_slice = similar(u_noisy, length(slices))
+    @inbounds for (i, slice) = enumerate(slices)
+        u_noisy_slice = select(u_noisy, slice; orientation=orientation)
+        u_ref_slice = select(u_ref, slice; orientation=orientation)
+        ssim_slice[i] = assess_ssim(u_noisy_slice, u_ref_slice)
+    end
+    return tuple(ssim_slice...)
+end
