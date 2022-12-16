a780ebfba027ac34dd13bff11cf404dd66846e79
Gabrio Rizzuti <rizzuti.gabrio@gmail.com>
2022-12-16 17:27:42 +0100

    Update

--- src/RetrospectiveMotionCorrectionMRI.jl
+++ src/RetrospectiveMotionCorrectionMRI.jl
@@ -1,6 +1,6 @@
 module RetrospectiveMotionCorrectionMRI
 
-using LinearAlgebra, AbstractLinearOperators, FastSolversForWeightedTV, AbstractProximableFunctions, UtilitiesForMRI, SparseArrays, ImageQualityIndexes
+using LinearAlgebra, AbstractLinearOperators, FastSolversForWeightedTV, AbstractProximableFunctions, UtilitiesForMRI, SparseArrays
 
 const RealOrComplex{T<:Real} = Union{T,Complex{T}}
 
@@ -8,7 +8,6 @@ abstract type AbstractImageReconstructionOptions end
 abstract type AbstractParameterEstimationOptions end
 abstract type AbstractMotionCorrectionOptions end
 
-include("./imagequality_utils.jl")
 include("./parameter_estimation.jl")
 include("./rigid_registration.jl")
 include("./image_reconstruction.jl")
